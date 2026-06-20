20a56ac8020293300cc47f9f495fcaf4bcec3917
Ruqi Chen <rqchen123@users.noreply.github.com>
2026-06-19 22:25:24 -0500

    Add linked list

--- chapter_eight/237_delete_node.py
+++ chapter_eight/237_delete_node.py
@@ -1,11 +1,23 @@
 import ut_node as ut
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 
 class Solution:
-    def deleteNode(self, node):
-        node.val = node.next.val
-        node.next = node.next.next
+    def deleteNode(self, head, node):
+        dummy = ListNode(0, head)
+        current = dummy
+        breakpoint()
+        while current.next:
+            if current.next == node:
+                current.next=current.next.next
+                break
+            current = current.next
 
+        return dummy.next
 
 # Build: 4 -> 5 -> 1 -> 9
 head = ut.build_linked_list([4, 5, 1, 9])
@@ -15,9 +27,10 @@ ut.print_linked_list(head)
 
 # Find the actual node object containing 5
 node_to_delete = ut.find_node(head, 5)
+print("node_to_delte:", node_to_delete)
 
 solution = Solution()
-solution.deleteNode(node_to_delete)
+head = solution.deleteNode(head, node_to_delete)
 
 print("After:")
 ut.print_linked_list(head)
