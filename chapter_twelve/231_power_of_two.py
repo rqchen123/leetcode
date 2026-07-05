404d8246f7f3c7473d835388b18961a3a2307549
Ruqi Chen <rqchen123@users.noreply.github.com>
2026-07-05 18:16:18 -0500

    Add leetcode 231

---
+++ chapter_twelve/231_power_of_two.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        while n > 1:
+            if n % 2 != 0:
+                return False
+            n = n // 2
+        return True
+
+
+
+n = 4
+#Output: true
+t = Solution()
+re = t.isPowerOfTwo(n)
+print(re)
