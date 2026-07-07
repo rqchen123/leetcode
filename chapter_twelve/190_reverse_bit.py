fdcba6742a4d6005e2406a0f1b7a0f68bd753d6a
Ruqi Chen <rqchen123@users.noreply.github.com>
2026-07-07 16:30:15 -0500

    Add reverse bit

--- chapter_twelve/190_reverse_bit.py
+++ chapter_twelve/190_reverse_bit.py
@@ -1,14 +1,14 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
-        
         for i in range(32):
-            last_bit =  n&1
-            result = (result << 1) | last_bit
-            n >> 1
-        return result 
+            last_bin = n&1
+            result = (result <<1) | last_bin
+            n = n >> 1
+        return result
 
 n = 1
+# output = 2147483648
 t = Solution()
 re = t.reverseBits(n)
 print(re)
